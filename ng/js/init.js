a359b56bce9d706704917098585728f8f834ca24
Marco Obermeyer <marco.obermeyer@obcon.de>
2016-11-16 17:39:14 +0100

    test ng

--- ng/js/init.js
+++ ng/js/init.js
@@ -2,6 +2,19 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.obcon_imprint').hide();
+    
+
+    $('.nav_obcon_imprint').click(function() {
+        $('.obcon_about').hide();
+        $('.obcon_imprint').show();
+    });
+
+    $('.nav_obcon_about').click(function() {
+        $('.obcon_imprint').hide();
+        $('.obcon_about').show();
+    });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
